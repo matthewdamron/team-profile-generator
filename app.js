e70cd8ebc0033d522a4036859507551a36470723
Matthew Damron <mattdamron@msn.com>
2021-01-23 10:34:36 -0600

    add manager prompts

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const Generator = require('./lib/Generator');
 
 new Generator().initializeGenerator();
+// generator = new Generator();
+// generator.initializeGenerator()
+// generator.employeeGenerator();
+
 
 // console.log(Generator);
